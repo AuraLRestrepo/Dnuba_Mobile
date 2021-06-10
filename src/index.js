9b7e6c495bf7561cd0564f79e72f7443de7f80c1
Aura Lucia <lara.restalvarez@gmail.com>
2021-06-10 09:08:51 -0500

    version 1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Hello_Word from './components/hello_word';
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-const container = document.getElementById('root');
+const containers = document.getElementById('root');
 
-ReactDOM.render(container);
+ReactDOM.render(containers);
 reportWebVitals();
