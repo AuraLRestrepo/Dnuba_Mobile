9a219a47af88621da07f2b2201914f7b525fe140
AuraLRestrepo <42555123+AuraLRestrepo@users.noreply.github.com>
2021-06-10 09:53:27 -0500

    commit

---
+++ src/components/hello_word.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Hello_Word extends React.Component {
+    render(){
+        return (
+            <h1>Hello Word desde React</h1>
+        );
+    }
+
+}
+
+export default Hello_Word
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Hello_Word from './components/hello_word';
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-const containers = document.getElementById('root');
+const container = document.getElementById('root');
 
-ReactDOM.render(containers);
+ReactDOM.render(<Hello_Word/>,container);
 reportWebVitals();
