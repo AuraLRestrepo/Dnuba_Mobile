d61015ad8f5a04da43a47ef65bc5900d4074014e
AuraLRestrepo <42555123+AuraLRestrepo@users.noreply.github.com>
2021-06-11 11:40:50 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import reportWebVitals from './reportWebVitals';
 import TasksProgress from './components/task_in_progress/task_in_progress';
 
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 const container = document.getElementById('root');
 
 ReactDOM.render(<TasksProgress/>,container);
